89dd941d690aeae3936ab6bb7f8736060561bc73
AlinaSikabonyi <155567586+AlinaSikabonyi@users.noreply.github.com>
2024-01-03 18:37:04 +0100

    Update try2.py

--- try2.py
+++ try2.py
@@ -5,26 +5,26 @@ from parameters import *
 
 #parameters for each pateint taken from parameters.py
 
-# HPf function definition
+#ODE system defniert input vektor output AUCH vektor 
 def ODEsystem(t, z, S, phi, A, alpha):
     x, y = z
     dx_dt = S / np.exp(phi * y) - x
     dy_dt = A - (A / np.exp(alpha * x)) - y
     return [dx_dt, dy_dt]
 
-
+#AWP
 x0 = [0, 0]
 
-
+#Zeitspanne
 t_span = (0, 50)
 
 
-
+#solver zumgenaurenauswerten entweder t-eval oder dense-out = true 
 sol = solve_ivp(ODEsystem, t_span, x0, args=(S, phi, A, alpha)) #t_eval=np.linspace(0, 50, 1000)
 
 # Plot both x(t) and y(t) in the same plot
-plt.plot(sol.t, sol.y[0], label='x(t)')
-plt.plot(sol.t, sol.y[1], label='y(t)')
+plt.plot(sol.t, sol.y[0], label='x(t)') #y[0] nach der ersten variable in diesem fall x also HP
+plt.plot(sol.t, sol.y[1], label='y(t)') #y[1] nach der zweiten variable in diesem fall y also TH
 plt.xlabel('Time')
 plt.ylabel('Values')
 plt.legend(['x', 'y'], shadow=True)
